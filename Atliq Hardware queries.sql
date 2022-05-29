802530afc5f4b4c5ebe935328014cd8e0e66b286
Akshay <104198324+Aky1999@users.noreply.github.com>
2022-05-29 12:34:51 +0530

    Add files via upload

---
+++ Atliq Hardware queries.sql
@@ -0,0 +1,54 @@
+use sales
+
+
+#all customer records
+
+SELECT * FROM customers;
+
+
+
+#total number of customers
+
+SELECT count(*) FROM customers;
+
+
+
+#transactions for Delhi market (market code for chennai is Mark001
+
+SELECT * FROM transactions where market_code='Mark004';
+
+
+
+#distrinct product codes that were sold in Delhi
+
+SELECT distinct product_code FROM transactions where market_code='Mark004';
+
+
+
+#transactions where currency is US dollars
+
+SELECT * from transactions where currency="USD"
+
+
+
+#transactions in 2020 join by date table
+
+SELECT transactions.*, date.* FROM transactions INNER JOIN date ON transactions.order_date=date.date where date.year=2020;
+
+
+
+#total revenue in year 2020,
+
+SELECT SUM(transactions.sales_amount) FROM transactions INNER JOIN date ON transactions.order_date=date.date where date.year=2020 and transactions.currency="INR\r" or transactions.currency="USD\r";
+
+
+
+#total revenue in year 2020, January Month,
+
+SELECT SUM(transactions.sales_amount) FROM transactions INNER JOIN date ON transactions.order_date=date.date where date.year=2020 and and date.month_name="January" and (transactions.currency="INR\r" or transactions.currency="USD\r");
+
+
+
+#total revenue in year 2020 in Chennai
+
+SELECT SUM(transactions.sales_amount) FROM transactions INNER JOIN date ON transactions.order_date=date.date where date.year=2020 and transactions.market_code="Mark001";
